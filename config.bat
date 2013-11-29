48038cc9046aedcfd08a8827d33649eeb8553b59
abing_hu <abing_hu@163.com>
2013-11-29 23:55:46 +0800

    create autoload folder

--- config.bat
+++ config.bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem 从vim-pathogen文件夹中拷贝pathogen.vim到autoload文件夹下
-copy vim-pathogen\autoload\pathogen.vim autoload
+mkdir autoload
+copy vim-pathogen\autoload\pathogen.vim autoload\pathogen.vim
 
 pause>nul
